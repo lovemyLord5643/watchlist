14a25a619b427659f92f9274305d0ec9ad302e95
lovemyLord5643 <840904755@qq.com>
2022-03-26 21:44:44 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from glob import escape
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/user/<name>")
@@ -7,8 +7,8 @@ def user_page(name):
     return 'User: %s' % escape(name)
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/test')
 def test_url_for():
@@ -18,6 +18,20 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return 'Test page'
+
+name = 'Roger Chen'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
  
